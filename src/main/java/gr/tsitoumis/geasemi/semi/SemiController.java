3c8db4bcd24fb5d1a22dbf7981a35fe80b6954ec
Konstantinos Tsitoumis <49339421+dinosts@users.noreply.github.com>
2022-06-19 16:28:54 +0300

    added removeProject and better error handling

--- src/main/java/gr/tsitoumis/geasemi/semi/SemiController.java
+++ src/main/java/gr/tsitoumis/geasemi/semi/SemiController.java
@@ -3,6 +3,7 @@ package gr.tsitoumis.geasemi.semi;
 import gr.tsitoumis.geasemi.semi.schemas.SemiRunRequestBody;
 import gr.tsitoumis.geasemi.semi.schemas.SemiRunResponseBody;
 import gr.tsitoumis.geasemi.services.CommandService;
+import gr.tsitoumis.geasemi.services.GeaSemiException;
 import gr.tsitoumis.geasemi.services.GitTools;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +18,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SemiController {
     @RequestMapping(value = "run", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<SemiRunResponseBody> run(@RequestBody SemiRunRequestBody request) {
+    public ResponseEntity<SemiRunResponseBody> run(@RequestBody SemiRunRequestBody request) throws Exception {
+        String url = request.getUrl();
+        String name = GitTools.getProjectName(request.getUrl());
+        String version = request.getVersion();
+        String lang = request.getLanguage();
+        
         try {
-            CommandService.gitClone(request.getUrl());
-            CommandService.semiRun(request);
+            CommandService.gitClone(url);
+
+            CommandService.semiRun(name, lang, version);
+
+            CommandService.deleteProject(name);
 
             return ResponseEntity.status(HttpStatus.OK).body(new SemiRunResponseBody("Semi analysis COMPLETED"));
-        } catch (Exception e) {
+        } catch (GeaSemiException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new SemiRunResponseBody("Semi analysis FAILED: " + e.getMessage()));
+        } catch (Exception e) {
+            CommandService.deleteProject(name);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new SemiRunResponseBody("Semi analysis FAILED"));
         }
     }
 }
--- src/main/java/gr/tsitoumis/geasemi/services/CommandService.java
+++ src/main/java/gr/tsitoumis/geasemi/services/CommandService.java
@@ -1,6 +1,7 @@
 package gr.tsitoumis.geasemi.services;
 
 import gr.tsitoumis.geasemi.semi.schemas.SemiRunRequestBody;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.io.*;
 import java.nio.file.Path;
@@ -30,11 +31,7 @@ public class CommandService {
     // TODO: CONFIG SECRETS
     private static String dbString = "195.251.210.147:3363 metrics metrics 308d139ce20d7695a301d723f8bf379d1fc7dd899966d8102578d4acc85bbf8e";
 
-    private static void runCmd(String cmd) throws Exception {
-        runCmd(cmd, "./gitRepositories");
-    }
-
-    private static void runCmd(String cmd, String directory) throws Exception {
+    private static int runCmd(String cmd, String directory) throws Exception {
 
         boolean isWindows = System.getProperty("os.name")
                 .toLowerCase().startsWith("windows");
@@ -55,36 +52,57 @@ public class CommandService {
 
 
         int exitCode = process.waitFor();
-        if (exitCode != 0) {
-            throw new Exception("Exit code not 0");
-        }
+
+        return exitCode;
     }
 
     // Git
 
     public static void gitClone(String project) throws Exception {
-        try {
-            runCmd("git clone " + project);
-        } catch (Exception e) {
-            throw new Exception("Git clone failed.");
+
+        int exitCode = runCmd("git clone " + project, "./gitRepositories");
+
+        if (exitCode == 128) {
+            throw new GeaSemiException("Project already cloned");
+        }
+
+        if (exitCode != 0) {
+            throw new GeaSemiException("Git clone FAILED");
         }
     }
 
     // Semi
-    //TODO: PLACE SEMI IN PROJ DIR
-    public static void semiRun(SemiRunRequestBody request) throws Exception {
+    public static void semiRun(String name, String lang, String version) throws Exception {
         try {
-            String name = GitTools.getProjectName(request.getUrl());
-            String lang = request.getLanguage();
-            String version = request.getVersion();
 
-            Path path = Paths.get("gitRepositories/" + name).toAbsolutePath();
+            Path projectPath = Paths.get("gitRepositories/" + name).toAbsolutePath();
+            Path semiPath = Paths.get("geasemiJars/semi-0.0.1-jar-with-dependencies.jar").toAbsolutePath();
+            Path semiExtractPosition = Paths.get("gitRepositories/" + name).toAbsolutePath();
 
-            String command = "java -jar semi-0.0.1-jar-with-dependencies.jar" + " " + lang + " " + name + " " + version + " " + path + " " + dbString;
+            String command = "java -jar" + " " + semiPath + " " + lang + " " + name + " " + version + " " + projectPath + " " + dbString;
 
-            runCmd(command);
+            int exitCode = runCmd(command, semiExtractPosition.toString());
+
+            if (exitCode != 0) {
+                throw new GeaSemiException("Semi analysis FAILED.");
+            }
+
         } catch (Exception e) {
             throw new Exception("semi analysis failed");
         }
     }
+
+
+    public static void deleteProject(String projectName) throws Exception {
+        boolean isWindows = System.getProperty("os.name")
+                .toLowerCase().startsWith("windows");
+
+        if (isWindows) {
+            runCmd("rd /s /q " + projectName, "./gitRepositories");
+        } else {
+            runCmd("rm -rf " + projectName, "./gitRepositories");
+        }
+    }
+
+
 }
